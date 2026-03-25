57d1afbf6c3e5c290a78d263e160435de6c1e31b
olDox333 <victoralexandregamer5@gmail.com>
2026-03-25 01:08:43 -0300

    Alfa 162.40 : 2026.03.25.a : ■ TERMUX-CONFIG - correções menores

--- doxoade/commands/termux_systems/termux_tools.py
+++ doxoade/commands/termux_systems/termux_tools.py
@@ -1,27 +1,23 @@
 # doxoade/commands/termux_systems/termux_tools.py
 
-import os
-import json
-import re
-from doxoade.tools.error_info import handle_error
-from doxoade.tools.exec_safe import run_safe
-
-
 def setup_extra_keys():
+    import os
     termux_dir = os.path.expanduser("~/.termux")
     prop_file = os.path.join(termux_dir, "termux.properties")
     os.makedirs(termux_dir, exist_ok=True)
 
     extra_keys_value = (
-        "extra-keys = [['ESC','SHIFT' ,'/','-','HOME','UP','END','PGUP'],"
-        "['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
+        "extra-keys = [['ESC','SHIFT' ,'/','*',';','HOME','UP','END','PGUP'],"
+        "['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN'],"
+        "['(',')','{','}','+','-',]]"
     )
 
     lines = []
     if os.path.exists(prop_file):
         with open(prop_file, "r", encoding="utf-8") as f:
             lines = f.readlines()
-
+            
+    import re
     new_lines = [line for line in lines if not re.match(r'^\s*#?\s*extra-keys\s*=', line)]
 
     if new_lines and not new_lines[-1].endswith("\n"):
@@ -31,7 +27,9 @@ def setup_extra_keys():
     with open(prop_file, "w", encoding="utf-8") as f:
         f.writelines(new_lines)
 
+    from doxoade.tools.exec_safe import run_safe
     try:
         run_safe("termux-reload-settings")
     except Exception as e:
+        from doxoade.tools.error_info import handle_error
         handle_error(e, context="termux-reload-settings")
